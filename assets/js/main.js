29eadefb4aced20ba4856a50250b1712118f9b3b
ebongso <ebongso@gmail.com>
2017-05-20 17:55:05 -0700

    added Lucky data and display Lucky items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,94 @@
-'use strict'
+(() => {
+  'use strict';
 
-let item = ({ x_large_image_url, discount_percent, current_price, sale_story, brand, name, description }) => 
-  ` 
-  <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 text-center">
-    <div class="card">
-      <div class="card-image" style="background-image: url('${x_large_image_url}')">
-        <p class="card-label-top-left">
-          <b style="color: #FFFFFF;">${discount_percent}% OFF</b>
-        </p>
-        <p class="card-label-bottom-right"><b><i>$${Number(current_price).toFixed(2)}</i></b></p>
-      </div>
-      <div class="card-description">
-        <small>${sale_story.split(',')[0]}</small>
-        <p><b>${brand.substr(0, 30)}<br />${name.substr(0, 60)}</b></p>
-        <small>${description.substr(0, 35)}</small>
+  let goitem = ({ x_large_image_url, discount_percent, current_price, sale_story, brand, name, description }) => 
+    ` 
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 text-center">
+      <div class="card">
+        <div class="card-image" style="background-image: url('${x_large_image_url}')">
+          <p class="card-label-top-left">
+            <b style="color: #FFFFFF;">${discount_percent}% OFF</b>
+          </p>
+          <p class="card-label-bottom-right"><b><i>${Number(current_price).toFixed(2)}</i></b></p>
+        </div>
+        <div class="card-description">
+          <small>${sale_story ? sale_story.split(',')[0] : ''}</small>
+          <p><b>${brand ? brand.substr(0, 30) : ''}<br />${name ? name.substr(0, 60) : ''}</b></p>
+          <small>${description ? description.substr(0, 35) : ''}</small>
+        </div>
       </div>
     </div>
-  </div>
-  `;
+    `;
 
-let req = new XMLHttpRequest();
-req.onreadystatechange = () => {
-  if(req.readyState == 4 && req.status == 200) {
-    var items = JSON.parse(req.responseText);
-    document.getElementById('grocery-items').innerHTML = items.map(item).join('');
+  let luckyitem = ({ ImageUrl, Price, ProductName, ProductDescription }) => 
+    ` 
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 text-center">
+      <div class="card">
+        <div class="card-image" style="background-image: url('${ImageUrl}')">
+          <p class="card-label-top-left ${(Price.split('<br>').length > 1 && Price.split('<br>')[1] !== 'of equal or' && Price.split('<br>')[1].indexOf('Buy') < 0) ? '' : 'hidden'}">
+            <b style="color: #FFFFFF;">${Price.indexOf('Buy') >= 0 ? Price.split('<br>').join(' ') : Price.split('<br>')[1]}</b>
+          </p>
+          <p class="card-label-bottom-right ${(Price.split('<br>')[0].indexOf('Buy') >= 0) ? 'hidden' : ''}"><b><i>${Price.split('<br>')[0].substr(0, 12)}</i></b></p>
+        </div>
+        <div class="card-description">
+          <p><b>${ProductName ? ProductName.substr(0, 60) : ''}</b></p>
+          <small>${ProductDescription ? ProductDescription.substr(0, 70) : ''}</small>
+        </div>
+      </div>
+    </div>
+    `;
+
+  const url = window.location.href;
+  console.log(url);
+  const splitUrl = url.split('?');
+  let storeName = '';
+  let page = '';
+  if(splitUrl.length > 1) {
+    let splitQuery = splitUrl[1].split('&');
+    for(let q of splitQuery) {
+      let splitQ = q.split('=');
+      if(splitQ[0] === 'store') storeName = splitQ[1];
+      else if(splitQ[0] === 'page') page = splitQ[1];
+    }
   }
-};
-req.open('GET', '/api/items', true);
-req.send();
+
+  //Make sure the store dropdown is selected
+  let storeSelect = document.getElementById('storeSelect');
+  for(let i, j = 0; i = storeSelect.options[j]; j++) {
+    if(i.value === storeName) {
+      storeSelect.selectedIndex = j;
+    }
+  }
+
+  //Load items by store with paging
+  let loadItems = (storeName, page) => {
+    let req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+      if(req.readyState == 4 && req.status == 200) {
+        var json = JSON.parse(req.responseText);
+        document.getElementById('grocery-items').innerHTML = json.items.map(storeName === 'lucky' ? luckyitem : goitem).join('');
+        createPaging(json.totalPages, page);
+      }
+    };
+    req.open('GET', '/api/items?store=' + storeName + '&page=' + page, true);
+    req.send();
+
+    let createPaging = (totalPages, currentPage) => {
+      if(totalPages > 1) {
+        let paging = '';
+        for(let i = 1; i <= totalPages; i++) {
+          paging += '<span class="paging' + (i == currentPage ? ' paging-active' : '' ) + 
+            '"><a href="/?store=' + storeName + '&page=' + i + '">' + i + '</a></span>';
+        }
+        document.getElementById('pagingTop').innerHTML = paging;
+        document.getElementById('pagingBottom').innerHTML = paging;
+      }
+    };
+  };
+  loadItems(storeName, page);
+
+  //Reload the page when the store selection changes
+  storeSelect.addEventListener('change', (e) => {
+    window.location = window.location.href.split('?')[0] + '?store=' + e.target.value;
+  });
+})();
--- routes/items.js
+++ routes/items.js
@@ -1,13 +1,46 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
 const mongojs = require('mongojs');
+const path = require('path');
 const router = express.Router();
 
-router.get('/items', (req, res, next) => {
-  req.db.collection('items').find((err, items) => {
-    if(err) return res.send(err);
+const GOitems = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/GOitems.json'), 'utf-8'));
+const Luckyitems = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/Luckyitems.json'), 'utf-8'));
 
-    res.json(items);
-  });
+router.get('/items', (req, res, next) => {
+  const store = req.query.store;
+  let page = req.query.page;
+  page = page === '' ? '1' : page;
+
+  // req.db.collection('items').find((err, items) => {
+  //   if(err) return res.send(err);
+
+  //   res.json(items);
+  // });
+
+  let data = {
+    totalPages: store === 'lucky' ? Luckyitems[0].CS_Page.length : 1,
+    items: store === 'lucky' ? Luckyitems[0].CS_Page.filter(c => c.PageName === page)[0].SaleItems : 
+      GOitems.items.filter(i => i.brand !== '')
+  };
+  res.json(data);
+});
+
+router.get('/items/load', (req, res, next) => {
+  console.log(GOitems);
+  res.json('done');
+});
+
+router.get('/items/refresh', (req, res, next) => {
+  const callback = (response) => {
+    let str = '';
+
+    response.on('data', (chunk) => str += chunk);
+    response.on('end', () => console.log(str));
+  };
+  http.request('http://flyers.groceryoutlet.com/flyer_data/1157470?locale=en-US', callback).end();
+  res.json('done');
 });
 
 router.get('/item/:id', (req, res, next) => {
